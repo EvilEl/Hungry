d4107a6525b0bf9aa490ebc2fd490754b3292b84
Mikhail <enc0ru@rambler.ru>
2020-10-15 22:24:46 +0300

    meniClick

--- js/script.js
+++ js/script.js
@@ -6,33 +6,17 @@ const dots = document.querySelectorAll('.dot'),
     link = document.querySelectorAll('.menu__link'),
     stock = document.querySelectorAll('.stock__link'),
     stockWr = document.querySelectorAll('.stock__wrappper');
-
-// let stockwr = []
-// stock.forEach(function (value) {
-
-// });
-
-/*
-stock[0].addEventListener('click', e => {
-    if (flex.index == fleba.index) {
-        stockWr[0].classList.toggle('activeFlex')  // Работает но выбирая по [] МАССИВ
-    } else {
-
+stock.forEach(function (value, i) {
+    value.onclick = () => {
+        stockWr.forEach(function (valuevr, g) {
+            if (i == g) {
+                stockWr[i].classList.toggle('activeFlex')
+            } else {
+                stockWr[g].classList.remove('activeFlex')
+            }
+        })
     }
-    console.log(fleba);
 })
-
-stock[1].addEventListener('click', e => {
-    // if (flex.index == fleba.index) {
-    stockWr[1].classList.toggle('activeFlex')
-
-    // } else {
-
-    // }
-    console.log(fleba);
-})
-*/
-
 burger.addEventListener('click', e => {
 
     burger.classList.toggle('active');
@@ -53,6 +37,7 @@ function linkmass() {
 for (let i = 0; i < link.length; i++) {
     link[i].onclick = linkmass;
 }
+
 dots[0].addEventListener('click', e => {
     slider[0].classList.remove('margin');
     slider[1].classList.remove('margin')
@@ -87,108 +72,18 @@ dots[2].addEventListener('click', e => {
     dots[1].style.background = '#333';
     dots[2].style.background = '#fff';
 })
-// function openbox(SOUPE) {
-//     display = document.getElementById('SOUPE').style.display;
-//     if (display == "none") {
-//         document.getElementById('SOUPE').style.display = "flex";
-//         document.getElementById('PIZZA').style.display = "none"
-//         document.getElementById('PASTA').style.display = "none"
-//         document.getElementById('DESER').style.display = "none"
-//         document.getElementById('WINE').style.display = "none"
-//         document.getElementById('BEER').style.display = "none"
-//         document.getElementById('DRINKS').style.display = "none"
-//     } else {
-//         document.getElementById('SOUPE').style.display = "none"
-//     }
-// }
 
-// function PIZZA(PIZZA) {
-//     display = document.getElementById('PIZZA').style.display;
-//     if (display == "none") {
-//         document.getElementById('PIZZA').style.display = "flex";
-//         document.getElementById('SOUPE').style.display = "none";
-//         document.getElementById('PASTA').style.display = "none";
-//         document.getElementById('DESER').style.display = "none";
-//         document.getElementById('WINE').style.display = "none";
-//         document.getElementById('BEER').style.display = "none";
-//         document.getElementById('DRINKS').style.display = "none";
-//     } else {
-//         document.getElementById('PIZZA').style.display = "none"
-//     }
-// }
+// dots.forEach(function (value, i) {
+//     value.onclick = () => {
+//         slider.forEach(function (valuevr, g) {
+//             if (i == g) {
+//                 slider[i].classList.add('margin')
+//                 slider[i].style.transform = 'translateX(100%)'
+//             } else {
+//                 slider[g].style.transform = 'translateX(-200%)'
+//                 slider[i].classList.remove('margin')
+//             }
+//         })
 
-// function PASTA(PASTA) {
-//     display = document.getElementById('PASTA').style.display;
-//     if (display == "none") {
-//         document.getElementById('PASTA').style.display = "flex";
-//         document.getElementById('PIZZA').style.display = "none"
-//         document.getElementById('SOUPE').style.display = "none"
-//         document.getElementById('DESER').style.display = "none"
-//         document.getElementById('WINE').style.display = "none"
-//         document.getElementById('BEER').style.display = "none"
-//         document.getElementById('DRINKS').style.display = "none"
-//     } else {
-//         document.getElementById('PASTA').style.display = "none"
 //     }
-// }
-
-// function DESER(DESER) {
-//     display = document.getElementById('DESER').style.display;
-//     if (display == "none") {
-//         document.getElementById('DESER').style.display = "flex";
-//         document.getElementById('PIZZA').style.display = "none"
-//         document.getElementById('SOUPE').style.display = "none"
-//         document.getElementById('PASTA').style.display = "none"
-//         document.getElementById('WINE').style.display = "none"
-//         document.getElementById('BEER').style.display = "none"
-//         document.getElementById('DRINKS').style.display = "none"
-//     } else {
-//         document.getElementById('DESER').style.display = "none"
-//     }
-// }
-
-// function WINE(WINE) {
-//     display = document.getElementById('WINE').style.display;
-//     if (display == "none") {
-//         document.getElementById('WINE').style.display = "flex";
-//         document.getElementById('PIZZA').style.display = "none"
-//         document.getElementById('SOUPE').style.display = "none"
-//         document.getElementById('DESER').style.display = "none"
-//         document.getElementById('PASTA').style.display = "none"
-//         document.getElementById('BEER').style.display = "none"
-//         document.getElementById('DRINKS').style.display = "none"
-//     } else {
-//         document.getElementById('WINE').style.display = "none"
-//     }
-// }
-
-// function BEER(BEER) {
-//     display = document.getElementById('BEER').style.display;
-//     if (display == "none") {
-//         document.getElementById('BEER').style.display = "flex";
-//         document.getElementById('PIZZA').style.display = "none"
-//         document.getElementById('SOUPE').style.display = "none"
-//         document.getElementById('DESER').style.display = "none"
-//         document.getElementById('WINE').style.display = "none"
-//         document.getElementById('PASTA').style.display = "none"
-//         document.getElementById('DRINKS').style.display = "none"
-//     } else {
-//         document.getElementById('BEER').style.display = "none"
-//     }
-// }
-
-
-// function DRINKS(DRINKS) {
-//     display = document.getElementById('DRINKS').style.display;
-//     if (display == "none") {
-//         document.getElementById('DRINKS').style.display = "flex";
-//         document.getElementById('PIZZA').style.display = "none"
-//         document.getElementById('SOUPE').style.display = "none"
-//         document.getElementById('DESER').style.display = "none"
-//         document.getElementById('WINE').style.display = "none"
-//         document.getElementById('BEER').style.display = "none"
-//         document.getElementById('PASTA').style.display = "none"
-//     } else {
-//         document.getElementById('DRINKS').style.display = "none"
-//     }
-// }
+// })
